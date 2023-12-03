f13321484b97eccbcb29d9f03f74f5232cf32c5f
cybuhh <cybuhh@users.noreply.github.com>
2023-12-03 08:31:11 +0100

    cleanup css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import reactLogo from '../assets/react.svg';
 import viteLogo from '/vite.svg';
 import styles from './index.module.css';
+import classNames from 'classnames';
 
 export default function Index() {
   const [count, setCount] = useState(0);
+  const handleOnClick = () => setCount((count) => count + 1);
 
   return (
     <section>
@@ -13,12 +15,14 @@ export default function Index() {
           <img src={viteLogo} className={styles['logo']} alt='Vite logo' />
         </a>
         <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className={`${styles['logo']} ${styles['react']}`} alt='React logo' />
+          <img src={reactLogo} className={classNames(styles['logo'], styles['react'])} alt='React logo' />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className={styles['card']}>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+      <h1 className={styles['header']}>Vite + React</h1>
+      <div>
+        <button className={styles['button']} onClick={handleOnClick}>
+          count is {count}
+        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
